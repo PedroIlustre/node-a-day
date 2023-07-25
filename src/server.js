3fe7b6cfcc624d22516f2616362d34d3f629d66c
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-07-25 17:22:41 -0300

    feat: create DELETE route, extract route param by regexp

--- src/server.js
+++ src/server.js
@@ -2,16 +2,26 @@ import http from 'node:http'
 import { json } from '../middlewares/json.js'
 import { routes } from './routes.js'
 
+// Query parameter - parametro enviado no url ex.: http://localhost:8080/users?userId=2 (nao usar dados sensívels, mais parameteros de paginação, ordenação, etc)
+// Route parameter - parametro enviado na rota ex.: http://localhost:8080/users/2 (usados para identificação de recurso. Mesmo assim, dados sensíveis NAO devem ser enviados)
+// Body request  - envio de informações de um formulário ex.: http://localhost:8080/users e lá na app tem o json body com os parametros da requisição
+
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     await json(req, res)
 
     const route = routes.find(route => {
-        return route.method == method && route.path == url
+        return route.method == method && route.path.test(url)
     })
     
     if (route) {
+        const routeParams = req.url.match(route.path)
+
+        req.params = { ...routeParams.groups }
+
+        // console.info(req.params)
+
         return route.handler(req, res)
     }
 
