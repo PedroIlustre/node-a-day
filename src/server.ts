1ca02bb432e122dc70f8e8f247d188ace03566aa
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-08-01 08:21:53 -0300

    feat: create knex type

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,12 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-	const transaction = await knex('transactions').insert({
-		id: crypto.randomUUID(),
-		title: 'some text',
-		amount: 1000
-	}).returning('*')
-
-	return transaction
-})
-
-app.get('/hello2', async () => {
-	const transaction = await knex('transactions')
-		.where('amount', 500)
-		.select('*')
-	return transaction
+// registering plugin
+app.register(transactionsRoutes, {
+	prefix: 'transactions'
 })
 
 app.listen({
