e153d4f7c7d388531638c01d76d5070234b78c13
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-08-02 16:22:28 -0300

    feat: create test to get user and get user with correct response

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from 'vitest'
+import { test, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -20,5 +20,45 @@ describe('Transactions routes', () => {
 				type: 'credit'
 			})
 			.expect(201)
+	})
+
+	test('User can do transactions', async () => {
+		const createTransactionResponse = await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'Transaction created by test',
+				amount: 2000,
+				type: 'credit'
+			})
+
+		const cookies = createTransactionResponse.get('Set-Cookie')
+		await request(app.server)
+			.get('/transactions')
+			.set('Cookie', cookies)
+			.expect(200)
+
+	})
+
+	test('User can do transactions with the expected body response', async () => {
+		const createTransactionResponse = await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'Transaction created by test',
+				amount: 2000,
+				type: 'credit'
+			})
+
+		const cookies = createTransactionResponse.get('Set-Cookie')
+		const getTransactionResponse = await request(app.server)
+			.get('/transactions')
+			.set('Cookie', cookies)
+			.expect(200)
+
+		expect(getTransactionResponse.body.transactions).toEqual([
+			expect.objectContaining({
+				title: 'Transaction created by test',
+				amount: 2000,
+			})
+		])
 	})
 })
