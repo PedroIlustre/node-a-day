bcdfa35ceb7d01faea36699c2b18b4b27bc69508
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-08-15 22:16:59 -0300

    feat: divide controller

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,8 @@
-import { fastify } from "fastify"
-import { z } from "zod"
-import { prisma } from "@/lib/prisma"
+import { apiRoutes } from "@/http/routes/api"
+import fastify from 'fastify'
 
 export const app = fastify()
 
-app.post('/users', async (request, reply) => {
-	const registerBodySchema = z.object({
-		name: z.string(),
-		email: z.string().email(),
-		password: z.string().min(6)
-	})
+app.register(apiRoutes)
 
-	const { name, email, password } = registerBodySchema.parse(request.body)
-
-	await prisma.user.create({
-		data: {
-			name, email,
-			password_hash: password
-		}
-	})
-
-	return reply.status(201).send()
-})
 
