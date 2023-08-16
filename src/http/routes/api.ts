3c59696f69d1fee71d49a84fbf2b31a3ec569987
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-08-15 22:17:12 -0300

    feat: divide controller

---
+++ src/http/routes/api.ts
@@ -0,0 +1,6 @@
+import { register } from "@/http/controllers/register.controller"
+import { FastifyInstance } from "fastify"
+
+export async function apiRoutes(app: FastifyInstance) {
+	app.post('/users', register)
+}
