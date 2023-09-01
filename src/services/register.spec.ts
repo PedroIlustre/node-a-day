83ac0e003898b0b73fe6cca56ab549471a5d2f3e
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-08-31 21:18:07 -0300

    feat: add first switch test

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,24 +1,26 @@
 import { compare } from 'bcryptjs'
 import { expect, describe, it } from 'vitest'
 import { RegisterService } from './register'
+import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register service', () => {
-	it('Should hash user password upon registration', async () => {
-		const registerService = new RegisterService({
-			async findByEmail(email) {
-				return null
-			},
+	it('Should be able to register', async () => {
+		const usersRepository = new InMemoryUsersRepository
+		const registerService = new RegisterService(usersRepository)
 
-			async create(data) {
-				return {
-					id: 'user-1',
-					name: data.name,
-					email: data.email,
-					password_hash: data.password_hash,
-					created_at: new Date(),
-				}
-			},
+		const { user } = await registerService.execute({
+			name: 'John Doe',
+			email: 'johndoe@gmail.com',
+			password: '123456',
 		})
+
+		expect(user.id).toEqual(expect.any(String))
+	})
+
+	it('Should hash user password upon registration', async () => {
+		const usersRepository = new InMemoryUsersRepository
+		const registerService = new RegisterService(usersRepository)
 
 		const { user } = await registerService.execute({
 			name: 'John Doe',
@@ -32,5 +34,27 @@ describe('Register service', () => {
 		)
 
 		expect(isPasswordCorrectlyHashed).toBe(true)
+	})
+
+	it('Should not be able to register with same email twice', async () => {
+		const usersRepository = new InMemoryUsersRepository
+		const registerService = new RegisterService(usersRepository)
+		const email = 'johndoe@gmail.com'
+
+		await registerService.execute({
+			name: 'John Doe',
+			email,
+			password: '123456',
+		})
+
+		expect(() =>
+			registerService.execute({
+				name: 'John Doe',
+				email,
+				password: '123456',
+			})
+		).rejects.toBeInstanceOf(UserAlreadyExistsError)
+
+
 	})
 })
